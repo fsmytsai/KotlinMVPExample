7f4ce469691a34c3da6f01489a1d74285aa3fe04
fsmytsai <msphausung1998@gmail.com>
2018-01-13 02:32:43 +0800

    Modify into login method

--- app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiStores.kt
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiStores.kt
@@ -1,21 +1,18 @@
 package com.fsmytsai.kotlin_retrofit_rxjava_sample.service.retrofit
 
-import com.fsmytsai.kotlin_retrofit_rxjava_sample.model.MyMarkers
+import com.fsmytsai.kotlin_retrofit_rxjava_sample.model.User
 import io.reactivex.Observable
 import retrofit2.http.*
 
 /**
- * Created by tsaiminyuan on 2017/12/15.
- */
+* Created by fsmytsai on 2017/12/15.
+*/
 interface ApiStores {
-    //    @GET("book/search")
-//    fun getMarkers(@Query("q")  name: String,
-//                       @Query("tag")  tag: String, @Query("start")  start: Int,
-//                       @Query("count")  count: Int): Observable<Book>
-    @GET("MarkerApi/GetMarkerList")
-    fun getMyMarkers(): Observable<MyMarkers>
+    @GET("api/getUserData")
+    fun getUserData(): Observable<User>
 
     @FormUrlEncoded
-    @POST("MarkerApi/get400ErrorByPost")
-    fun get400ErrorByPost(@Field("DataList[]") DataList: List<String>): Observable<String>
+    @POST("api/login")
+    fun login(@Field("account") account: String,
+              @Field("password") password: String): Observable<String>
 }
--- app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/ui/activity/MainActivity.kt
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/ui/activity/MainActivity.kt
@@ -3,17 +3,16 @@ package com.fsmytsai.kotlin_retrofit_rxjava_sample.ui.activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.EditText
 import com.fsmytsai.kotlin_retrofit_rxjava_sample.R
-import com.fsmytsai.kotlin_retrofit_rxjava_sample.model.MyMarkers
+import com.fsmytsai.kotlin_retrofit_rxjava_sample.model.User
 import com.fsmytsai.kotlin_retrofit_rxjava_sample.service.presenter.MainPresenter
-import com.fsmytsai.kotlin_retrofit_rxjava_sample.service.view.GenericView
-import java.util.*
+import com.fsmytsai.kotlin_retrofit_rxjava_sample.service.view.MainView
+import kotlin.collections.ArrayList
 
-class MainActivity : AppCompatActivity() {
-    var tv_GetResult: TextView? = null
-    var tv_PostResult: TextView? = null
+class MainActivity : AppCompatActivity(), MainView {
+    var et_main_account: EditText? = null
+    var et_main_password: EditText? = null
 
     private var mainPresenter: MainPresenter? = null
 
@@ -21,12 +20,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        mainPresenter = MainPresenter(myMarkersView, test400ErrorView)
+        mainPresenter = MainPresenter(this)
     }
 
     private fun initViews() {
-        tv_GetResult = findViewById(R.id.tv_GetResult)
-        tv_PostResult = findViewById(R.id.tv_PostResult)
+        et_main_account = findViewById(R.id.et_main_account)
+        et_main_password = findViewById(R.id.et_main_password)
     }
 
     override fun onStop() {
@@ -39,37 +38,28 @@ class MainActivity : AppCompatActivity() {
         mainPresenter?.onDestroy()
     }
 
-    fun testGet(v: View) {
-        mainPresenter?.getMyMarkers()
+    fun login(v: View) {
+        mainPresenter?.login(et_main_account?.text.toString(), et_main_password?.text.toString())
     }
 
-    fun testPost(v: View) {
-        val dataList = ArrayList<String>()
-        dataList.add("Hello  ")
-        dataList.add("World  ")
-        dataList.add("!")
-
-        mainPresenter?.get400ErrorByPost(dataList)
+    override fun onFailure(errorList: ArrayList<String>) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private val myMarkersView = object : GenericView<MyMarkers> {
-        override fun onSuccess(m: MyMarkers) {
-            val rand = Random().nextInt(m.MarkerList.size)
-            tv_GetResult?.text = m.MarkerList[rand].Content
-        }
-
-        override fun onError(errorMessage: String) {
-            Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_SHORT).show()
-        }
+    override fun loginSuccess(resMessage: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 
-    private val test400ErrorView = object : GenericView<String> {
-        override fun onSuccess(m: String) {
-            //return 200
-        }
-
-        override fun onError(errorMessage: String) {
-            tv_PostResult?.text = errorMessage
-        }
+    override fun loginFinish() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
+
+    override fun getUserDataSuccess(userData: User) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getUserDataFinish() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
 }
