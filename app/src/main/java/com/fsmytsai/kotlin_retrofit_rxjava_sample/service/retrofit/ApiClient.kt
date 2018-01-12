587583120d1440b9ab18b7ed9b7d063aec08fcb2
fsmytsai <msphausung1998@gmail.com>
2018-01-13 02:30:34 +0800

    Delete init

--- app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiClient.kt
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiClient.kt
@@ -7,27 +7,21 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 /**
- * Created by tsaiminyuan on 2017/12/15.
- */
+* Created by fsmytsai on 2017/12/15.
+*/
 class ApiClient private constructor() {
-    private val client = OkHttpClient()
-    private val factory = GsonConverterFactory.create(GsonBuilder().create())
-    private val retrofit: Retrofit
+    private val mRetrofit: Retrofit = Retrofit.Builder ()
+            .baseUrl("https://laravelcommunity.azurewebsites.net/")
+            .client(OkHttpClient())
+            .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
 
     companion object {
         val instance: ApiClient by lazy { ApiClient() }
     }
 
-    init {
-        retrofit = Retrofit.Builder ()
-                .baseUrl("https://frogcroak.azurewebsites.net/api/")
-                .client(client)
-                .addConverterFactory(factory)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .build()
-    }
-
     fun getServer(): ApiStores {
-        return retrofit.create(ApiStores::class.java)
+        return mRetrofit.create(ApiStores::class.java)
     }
 }
