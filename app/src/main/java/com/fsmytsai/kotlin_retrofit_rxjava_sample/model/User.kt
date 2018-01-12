50963246df32b7e0fba24c2d758b7cc6e3f93d79
fsmytsai <msphausung1998@gmail.com>
2018-01-13 01:57:32 +0800

    Add Model User

---
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/model/User.kt
@@ -0,0 +1,11 @@
+package com.fsmytsai.kotlin_retrofit_rxjava_sample.model
+
+/**
+* Created by fsmytsai on 2018/1/13.
+*/
+data class User(
+        val account: String,
+        val name: String,
+        val email: String,
+        val profile_pic: String
+)
