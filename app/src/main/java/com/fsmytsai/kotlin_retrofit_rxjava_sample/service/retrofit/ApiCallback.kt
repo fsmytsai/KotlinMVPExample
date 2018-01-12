d5f32bfe21a3450e2a855bacd4571c9b2ff574e9
fsmytsai <msphausung1998@gmail.com>
2018-01-13 02:28:48 +0800

    Uncouple view

--- app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiCallback.kt
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiCallback.kt
@@ -1,26 +1,26 @@
 package com.fsmytsai.kotlin_retrofit_rxjava_sample.service.retrofit
 
-import com.fsmytsai.kotlin_retrofit_rxjava_sample.service.view.GenericView
+import com.google.gson.Gson
 import io.reactivex.observers.DisposableObserver
 import retrofit2.HttpException
 import java.net.UnknownHostException
 
+
 /**
- * Created by tsaiminyuan on 2017/12/17.
- */
-class ApiCallback<M>(private val genericView: GenericView<M>) : DisposableObserver<M>() {
+* Created by fsmytsai on 2017/12/17.
+*/
+abstract class ApiCallback<M> : DisposableObserver<M>() {
 
     override fun onComplete() {
-
+        onFinish()
     }
 
-    override fun onNext(t: M) {
-        if (t != null) {
-            genericView.onSuccess(t)
-        }
+    override fun onNext(model: M) {
+        onSuccess(model)
     }
 
     override fun onError(t: Throwable?) {
+        val errorList = ArrayList<String>()
         when (t) {
             is HttpException -> {
                 val code = t.code()
@@ -29,10 +29,19 @@ class ApiCallback<M>(private val genericView: GenericView<M>) : DisposableObserv
                     msg = "伺服器異常，請稍後再試"
                 else if (code == 500)
                     msg = "伺服器錯誤，請聯絡開發人員"
-                genericView.onError(msg!!)
+                errorList.add(msg!!)
+                onFailure(errorList)
             }
-            is UnknownHostException -> genericView.onError("請檢查網路連線")
-            else -> genericView.onError(t.toString())
+            is UnknownHostException -> {
+                errorList.add("請檢查網路連線")
+                onFailure(errorList)
+            }
+            else -> onFailure(Gson().fromJson(t.toString(), ArrayList<String>()::class.java))
         }
     }
+    abstract fun onSuccess(model: M)
+
+    abstract fun onFailure(errorList: ArrayList<String>)
+
+    abstract fun onFinish()
 }
