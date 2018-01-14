9dccc8d66039adee099fc6186dfcb4a13085b4d4
fsmytsai <msphausung1998@gmail.com>
2018-01-14 16:23:20 +0800

    Add logout

--- app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/view/MainView.kt
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/view/MainView.kt
@@ -3,10 +3,12 @@ package com.fsmytsai.kotlin_retrofit_rxjava_sample.service.view
 import com.fsmytsai.kotlin_retrofit_rxjava_sample.model.User
 
 /**
-* Created by fsmytsai on 2017/12/15.
-*/
+ * Created by fsmytsai on 2017/12/15.
+ */
 interface MainView : BaseView {
-    fun loginSuccess(resMessage: String)
+    fun loginSuccess(token: String)
     fun loginFinish()
     fun getUserDataSuccess(userData: User)
+    fun logoutSuccess(resMessage: String)
+    fun logoutFinish()
 }
