50db6492d42789bdcbf3ea060e5cb806ca301004
fsmytsai <msphausung1998@gmail.com>
2018-01-14 16:20:38 +0800

    Add AuthInterceptor

--- app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiClient.kt
+++ app/src/main/java/com/fsmytsai/kotlin_retrofit_rxjava_sample/service/retrofit/ApiClient.kt
@@ -1,18 +1,26 @@
 package com.fsmytsai.kotlin_retrofit_rxjava_sample.service.retrofit
 
+import com.fsmytsai.kotlin_retrofit_rxjava_sample.service.app.SharedService
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.IOException
 
 /**
-* Created by fsmytsai on 2017/12/15.
-*/
+ * Created by fsmytsai on 2017/12/15.
+ */
 class ApiClient private constructor() {
-    private val mRetrofit: Retrofit = Retrofit.Builder ()
+    private val mOkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(AuthInterceptor())
+            .build()
+
+    private val mRetrofit: Retrofit = Retrofit.Builder()
             .baseUrl("https://laravelcommunity.azurewebsites.net/")
-            .client(OkHttpClient())
+            .client(mOkHttpClient)
             .addConverterFactory(GsonConverterFactory.create(GsonBuilder().create()))
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
@@ -23,5 +31,21 @@ class ApiClient private constructor() {
 
     fun getServer(): ApiStores {
         return mRetrofit.create(ApiStores::class.java)
+    }
+
+    private class AuthInterceptor : Interceptor {
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): Response {
+            var request = chain.request()
+            val token = SharedService.mHttpDataSP?.getString("token", "")
+            try {
+                request = request.newBuilder().addHeader("Authorization", "Bearer " + token).build()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+
+            return chain.proceed(request)
+        }
     }
 }
